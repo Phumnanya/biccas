b19b892f94f273578cec72210d5ef0a6d4852e0f
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-12-09 22:13:44 +0100

    updated screen sizing

--- app/components/heading.tsx
+++ app/components/heading.tsx
@@ -4,36 +4,37 @@ export default function Heading() {
     return(
         <>
         {/* main flex container */}
-        <header className="w-full h-10 relative top-5 left[130px] rounded-[10px]
-        flex flex-row justify-between items-center">
-            <div className="text-biccas w-1/4">
-                <h1 className="font-semibold text-[50px] leading-[30px] tracking-normal"
+        <header className="w-full h-fit md:w-[1180px] md:h-10 relative top-5 
+        md:left[130px] rounded-[10px]
+        flex flex-row justify-between items-center mx-auto">
+            <div className="text-biccas md:w-40 md:h-[30px]">
+                <h1 className="font-semibold md:text-[50px] text-3xl md:leading-[30px] 
+                md:tracking-normal"
                 >Biccas</h1>
             </div>
-            {/* flex container for nav and login */}
-            <div className="flex flex-row justify-between items-center w-2/3">
-                {/* navigation links */}
-                <nav className="w-2/3 flex flex-row justify-evenly">
-                    <Link href="#" className="font-medium text-lg text-black">Home</Link>
-                    <Link href="#" className="font-medium text-base text-links">Product</Link>
-                    <Link href="#" className="font-medium text-base text-links">FAQ</Link>
-                    <Link href="#" className="font-medium text-base text-links">Blog</Link>
-                    <Link href="#" className="font-medium text-base text-links">About Us</Link>
-                </nav>
-                {/*login and signup */}
-                <div className="w-1/3 h-10 flex flex-row justify-center items-center">
-                    <div>
-                        <button type="button" className="text-base font-medium p-2
-                        text-links">
-                            Login
-                        </button>
-                    </div>
-                    <div>
-                        <button type="button" className="text-base font-medium p-2
-                        text-white bg-biccas rounded-[10px]">
-                            Sign Up
-                        </button>
-                    </div>
+            {/* navigation links */}
+            <nav className="flex flex-row md:justify-evenly md:w-[408px] md:h-[22px] 
+            relative md:left[629px] justify-between">
+                <Link href="#" className="md:font-medium md:text-lg text-black">Home</Link>
+                <Link href="#" className="md:font-medium md:text-base text-links">Product</Link>
+                <Link href="#" className="md:font-medium md:text-base text-links">FAQ</Link>
+                <Link href="#" className="md:font-medium md:text-base text-links">Blog</Link>
+                <Link href="#" className="md:font-medium md:text-base text-links">About Us</Link>
+            </nav>
+            {/*login and signup */}
+            <div className="md:w-[143px] md:h-10 relative md:flex hidden flex-row justify-center 
+            items-center md:left[1167px] rounded-[10px] w-1/3">
+                <div>
+                    <button type="button" className="md:text-base md:font-medium md:p-2
+                    text-links">
+                        Login
+                    </button>
+                </div>
+                <div>
+                    <button type="button" className="md:text-base md:font-medium md:p-2
+                    text-white bg-biccas rounded-[10px]">
+                        Sign Up
+                    </button>
                 </div>
             </div>
         </header>
