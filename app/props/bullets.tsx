870687380b1e5f1f9c5d3b0b620aca3349776b3e
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-12-09 22:20:47 +0100

    edited size

--- app/props/bullets.tsx
+++ app/props/bullets.tsx
@@ -8,7 +8,7 @@ type props = {
 
 export default function Bullets({one : one, two, three, four, five}: props) {
     return(
-        <div className="font-medium text-lg text-productivity">
+        <div className="font-medium md:text-lg text-productivity">
             <ul className="space-y-3">
                 <li className="flex items-center gap-3">
                     <img src="/Subtract.svg" className="w-5 h-5" />
