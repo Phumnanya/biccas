79cb78d59b5457037ff4066b55e03716ee356697
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-12-09 22:22:43 +0100

    Update planbtn.tsx

--- app/props/planbtn.tsx
+++ app/props/planbtn.tsx
@@ -5,7 +5,7 @@ type props = {
 export default function Planbutton({prop : prop}: props) {
     return(
         <button type="button" className="w-[260px] h-16 rounded-[20px] text-biccas
-        shadow-[0px_4px_9px_0px_rgba(0,0,0,0.05)] font-semibold text-lg text-center">
+        shadow-[0px_4px_9px_0px_rgba(0,0,0,0.05)] font-semibold md:text-lg text-center">
             {prop}
         </button>
     )
