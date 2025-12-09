4ba37bbff73e0511df11397b49c243cd2fce2500
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-12-09 22:12:21 +0100

    screen resolution

--- app/components/features.tsx
+++ app/components/features.tsx
@@ -1,15 +1,16 @@
 export default function Features() {
     return(
-        <div className="flex flex-row justify-between items-center w-full">
+        <div className="flex md:flex-row flex-col justify-between md:items-center 
+        items-start w-full">
             {/* Our features intro */}
-            <div className="w-1/4">
-                <p className="font-semibold text-5xl text-productivity">
-                    Our Features you can get
-                </p>
+            <div className="md:w-1/4 w-full">
+                <p className="font-semibold md:text-5xl text-2xl text-productivity
+                pb-4 md:pb-0"
+                >Our Features you can get</p>
             </div>
             {/* We offer */}
-            <div className="w-1/3">
-                <p className="font-medium text-lg text-links">
+            <div className="md:w-1/3 w-full pb-4 md:pb-0">
+                <p className="font-medium md:text-lg text-links">
                     We offer a variety of interesting features that you can help 
                     increase yor productivity at work and manage your projrct esaly
                 </p>
@@ -17,8 +18,8 @@ export default function Features() {
             {/* Get started btn */}
             <div className="">
                 <div>
-                    <button type="button" className="text-lg font-medium
-                    text-white rounded-[70px] bg-biccas px-7 py-5">
+                    <button type="button" className="text-lg font-medium px-4 py-2
+                    text-white rounded-[70px] bg-biccas md:px-7 md:py-5">
                         Get Started
                     </button>
                 </div>
