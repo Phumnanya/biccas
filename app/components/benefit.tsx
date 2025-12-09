eb201aec7f7323e05738baaaa8ba46f1a8daeeda
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-12-09 21:58:05 +0100

    added responsiveness

--- app/components/benefit.tsx
+++ app/components/benefit.tsx
@@ -5,13 +5,13 @@ export default function Benefits() {
         <section className="flex flex-row justify-between my-40">
             <div className="flex flex-col">
                 {/* what Benifit header */}
-                <header className="w-[429px] h-[122px]">
-                    <p className="font-bold text-5xl text-productivity">
+                <header className="md:w-[429px] md:h-[122px]">
+                    <p className="font-bold md:text-5xl text-2xl text-productivity">
                         What Benifit Will You Get
                     </p>
                 </header>
                 {/* Benifit bullet points */}
-                <div className="w-[471px] font-medium text-lg text-productivity">
+                <div className="md:w-[471px] font-medium md:text-lg text-productivity">
                     <ul className="space-y-3">
                         <li className="flex items-center gap-3">
                             <img src="/Subtract.svg" className="w-5 h-5" />
@@ -38,7 +38,7 @@ export default function Benefits() {
                 <br />
             </div>
             {/* Laptop image */}
-            <div className="relative w-[450px]">
+            <div className="relative md:w-[450px] w-[200px]">
                 <Image src="/What Benifit.png" alt="Laptop" fill className="object-cover" />
             </div>
         </section>
