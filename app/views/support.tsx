5ac401979ff5425a3780203ec57eeeb8e413ef70
Eustace Mbanefo <111151661+Phumnanya@users.noreply.github.com>
2025-12-09 22:26:59 +0100

    Update support.tsx

--- app/views/support.tsx
+++ app/views/support.tsx
@@ -3,8 +3,8 @@ import Analytics from "../components/analytics"
 
 export default function Support() {
     return(
-        <div className="bg-support w-full h-fit flex flex-row justify-between px-10
-        py-20">
+        <div className="bg-support w-full h-fit flex md:flex-row justify-between px-10
+        md:py-20 py-10 flex-col">
             <Ratings />
             <Analytics />
         </div>
